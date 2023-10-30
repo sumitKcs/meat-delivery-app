f0f018bf48c5179cc36e5c79a0e3685556bae3b2
sumit <itssumit258@gmail.com>
2023-10-30 23:05:34 +0530

    fix: header ul items

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,13 @@
 window.addEventListener("DOMContentLoaded", () => {
   const carousel_wrapper = document.querySelectorAll(".carousel_wrapper");
   const media_scroller = document.querySelectorAll(".media_scroller");
+  const offers_scroller = document.getElementById("offer_scroller");
   const next = document.querySelectorAll(".next");
   const prev = document.querySelectorAll(".prev");
 
   carousel_wrappers = Array.from(carousel_wrapper);
   carousel_wrappers.forEach((element) => {
     const nav_buttons = element.lastElementChild;
-    console.log(nav_buttons);
     if (nav_buttons) {
       const element_height = element.clientHeight;
       const nav_buttons_height = element_height / 3;
@@ -35,3 +35,34 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+// function autoScrollImageScroller() {
+//   const scrollWidth = offers_scroller.scrollWidth;
+//   const containerWidth = offers_scroller.clientWidth;
+//   console.log(offers_scroller);
+//   // Set the initial scroll position to 0
+//   let scrollPosition = 0;
+
+//   // Define the scroll speed (adjust this value as needed)
+//   const scrollSpeed = 2; // Pixels scrolled per frame
+//   const pauseDuration = 3000; //pause duration
+
+//   function animate() {
+//     if (scrollPosition < scrollWidth - containerWidth) {
+//       scrollPosition += scrollSpeed;
+//     } else {
+//       scrollPosition = 0; // Reset to the beginning
+//       setTimeout(function () {
+//         requestAnimationFrame(animate); // Resume scrolling after the pause
+//       }, pauseDuration);
+//       return; // Exit the function to pause scrolling
+//     }
+
+//     imageScroller.scrollLeft = scrollPosition;
+//     requestAnimationFrame(animate);
+//   }
+
+//   animate(); // Start the animation loop
+// }
+
+// autoScrollImageScroller();
