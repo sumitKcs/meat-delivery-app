ecc8d2e890b804627b0d123c1510e99d38b6dcac
sumit <itssumit258@gmail.com>
2023-11-02 18:40:14 +0530

    feat. activated filter button sortings

--- assets/js/app.js
+++ assets/js/app.js
@@ -155,3 +155,143 @@ function returnsTabHandler(e) {
   returns_tab.style.borderBottom = "3px solid var(--primary-color)";
   orders_tab.style.borderBottom = "none";
 }
+
+// Sort the card items based on price low to high
+const price_low_to_high = document.getElementById("price_low_to_high");
+if (price_low_to_high) {
+  price_low_to_high.addEventListener("click", function () {
+    // Get the card container and all the card items
+    let cardContainers = document.querySelectorAll(".food_card_grid");
+    cardContainers = Array.from(cardContainers);
+    cardContainers.forEach((cardContainer) => {
+      const cardItems = Array.from(
+        cardContainer.querySelectorAll(".food_card_item")
+      );
+
+      // Sort the card items based on price low to high
+      cardItems.sort((a, b) => {
+        const priceA = parseInt(
+          a.querySelector(".price").textContent.replace(/[^0-9]/g, ""),
+          10
+        );
+        const priceB = parseInt(
+          b.querySelector(".price").textContent.replace(/[^0-9]/g, ""),
+          10
+        );
+
+        return priceA - priceB;
+      });
+
+      // Clear the card container
+      cardContainer.innerHTML = "";
+
+      // Append the sorted card items back to the container
+      cardItems.forEach((card) => {
+        cardContainer.appendChild(card);
+      });
+    });
+  });
+}
+
+// Sort the card items based on price low to high
+const price_high_to_low = document.getElementById("price_high_to_low");
+if (price_high_to_low) {
+  price_high_to_low.addEventListener("click", function () {
+    // Get the card container and all the card items
+    let cardContainers = document.querySelectorAll(".food_card_grid");
+    cardContainers = Array.from(cardContainers);
+    cardContainers.forEach((cardContainer) => {
+      const cardItems = Array.from(
+        cardContainer.querySelectorAll(".food_card_item")
+      );
+
+      // Sort the card items based on price low to high
+      cardItems.sort((a, b) => {
+        const priceA = parseInt(
+          a.querySelector(".price").textContent.replace(/[^0-9]/g, ""),
+          10
+        );
+        const priceB = parseInt(
+          b.querySelector(".price").textContent.replace(/[^0-9]/g, ""),
+          10
+        );
+
+        return priceB - priceA;
+      });
+
+      // Clear the card container
+      cardContainer.innerHTML = "";
+
+      // Append the sorted card items back to the container
+      cardItems.forEach((card) => {
+        cardContainer.appendChild(card);
+      });
+    });
+  });
+}
+
+// Sort the food card items on alphabetical order A-Z
+const receipe_a_to_z = document.getElementById("receipe_a_to_z");
+if (receipe_a_to_z) {
+  receipe_a_to_z.addEventListener("click", function () {
+    // Get the card container and all the card items
+    let cardContainers = document.querySelectorAll(".food_card_grid");
+    cardContainers = Array.from(cardContainers);
+    cardContainers.forEach((cardContainer) => {
+      const cardItems = Array.from(
+        cardContainer.querySelectorAll(".food_card_item")
+      );
+
+      // Sort the card items based on the recipe title (A-Z)
+      cardItems.sort((a, b) => {
+        const recipeTitleA = a.querySelector("#recipe_title").textContent;
+        const recipeTitleB = b.querySelector("#recipe_title").textContent;
+
+        return recipeTitleA.localeCompare(recipeTitleB, "en", {
+          sensitivity: "base",
+        });
+      });
+
+      // Clear the card container
+      cardContainer.innerHTML = "";
+
+      // Append the sorted card items back to the container
+      cardItems.forEach((card) => {
+        cardContainer.appendChild(card);
+      });
+    });
+  });
+}
+
+// Sort the food card items on alphabetical order Z-A
+const receipe_z_to_a = document.getElementById("receipe_z_to_a");
+if (receipe_z_to_a) {
+  receipe_z_to_a.addEventListener("click", function () {
+    // Get the card container and all the card items
+    let cardContainers = document.querySelectorAll(".food_card_grid");
+    cardContainers = Array.from(cardContainers);
+    cardContainers.forEach((cardContainer) => {
+      const cardItems = Array.from(
+        cardContainer.querySelectorAll(".food_card_item")
+      );
+
+      // Sort the card items based on the recipe title (Z-A)
+      cardItems.sort((a, b) => {
+        const recipeTitleA = a.querySelector("#recipe_title").textContent;
+        const recipeTitleB = b.querySelector("#recipe_title").textContent;
+
+        return recipeTitleB.localeCompare(recipeTitleA, "en", {
+          sensitivity: "base",
+        });
+      });
+
+      // Clear the card container
+      cardContainer.innerHTML = "";
+
+      // Append the sorted card items back to the container
+      cardItems.forEach((card) => {
+        cardContainer.appendChild(card);
+      });
+    });
+  });
+}
